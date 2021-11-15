61d3451b60059cfa4c469e450db0b262ce0a4d6e
Edwin Kofler <edwin@kofler.dev>
2021-11-14 19:19:58 -0800

    feat: Player flips direction on movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,13 +8,13 @@ public class Movement : MonoBehaviour
 {
 
     private new Transform transform;
-
+    private SpriteRenderer spriteRenderer;
     private Tilemap tileMap;
 
     void Start()
     {
         transform = GetComponent<Transform>();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
         tileMap = GameObject.Find("TerrainMap").GetComponent<Tilemap>();
     }
 
@@ -22,21 +22,25 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            spriteRenderer.flipX = true;
             Move(-1, 0, false);
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            spriteRenderer.flipX = false;
             Move(1, 0, false);
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            spriteRenderer.flipX = true;
             Move(0, -1, false);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            spriteRenderer.flipX = false;
             Move(0, 1, false);
         }
 
