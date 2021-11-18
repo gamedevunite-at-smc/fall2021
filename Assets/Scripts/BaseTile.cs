c01b35d4197dfdf136cc65e3edda67cad63aa650
OfficialFoneE <76148395+OfficialFoneE@users.noreply.github.com>
2021-11-17 22:04:07 -0800

    Movement atempt lol

--- Assets/Scripts/BaseTile.cs
+++ Assets/Scripts/BaseTile.cs
@@ -6,17 +6,19 @@ using UnityEngine.Tilemaps;
 
 
 
+[System.Serializable]
+public enum Direction
+{
+    None, UpRight, UpLeft, DownRight, DownLeft
+}
+
 public class BaseTile : Tile
 {
 
     public bool isRamp;
 
-    public enum RampDirection 
-    { 
-        UpRight, UpLeft, DownRight, DownLeft
-    }
 
-    public RampDirection rampDirection;
+    public Direction rampDirection;
 
     //public void BaseTile(Tile tile)
     //{
